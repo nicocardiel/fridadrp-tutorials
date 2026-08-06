c211895e74ba7c673e8fb40b922ef625d39958c7
Nicolás Cardiel <cardiel@ucm.es>
2026-08-06 16:44:11 +0200

    Third part of the documentation concerning the determination of the initial slice boundaries

--- tutorials/auxiliary_tools/initial_slice_boundaries/files/run_extract_blocks_ansi_helps.sh
+++ tutorials/auxiliary_tools/initial_slice_boundaries/files/run_extract_blocks_ansi_helps.sh
@@ -7,6 +7,7 @@ local_dir="`pwd`/tutorials/auxiliary_tools/initial_slice_boundaries/files"
 #./extract_ansi_help1.sh ${local_dir} fridadrp-find_slice_boundary_borders_from_flat
 ./extract_ansi_help2.sh ${local_dir} fridadrp-find_slice_boundary_borders_from_flat
 ./extract_ansi_help2.sh ${local_dir} fridadrp-find_traces_within_slice_boundary_polynomials
+./extract_ansi_help2.sh ${local_dir} fridadrp-interpolate_traces_within_slices
 ./extract_ansi_help2.sh ${local_dir} fridadrp-fit_slice_boundary_borders_with_polynomials
 ./extract_ansi_help2.sh ${local_dir} fridadrp-overplot_slice_boundary_polynomials
 ./extract_ansi_help2.sh ${local_dir} fridadrp-predict_polynomial_slice_borders
